7c50208d1d61c4189f37a4d0201d2daf1f944aff
ramu_annam <ramuannam121@gmail.com>
2024-07-04 15:28:44 +0530

    updated_FakeStoreAPI

--- backendProject/src/main/java/com/speedlink/backendproject/dtos/FakeStoreProductDto.java
+++ backendProject/src/main/java/com/speedlink/backendproject/dtos/FakeStoreProductDto.java
@@ -1,5 +1,10 @@
 package com.speedlink.backendproject.dtos;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class FakeStoreProductDto { // this is replica/ intermediary which is similar to client/ backend api data format or blueprint to exchange  the data between them.
   private  Long id;
   private String title;
--- backendProject/src/main/java/com/speedlink/backendproject/services/FakeStoreProductService.java
+++ backendProject/src/main/java/com/speedlink/backendproject/services/FakeStoreProductService.java
@@ -22,8 +22,9 @@ public class FakeStoreProductService implements ProductService { // so here comp
         //so using RestTemplate (which is a class in spring web) allow you to make call http to third party systems.
         //   RestTemplate restTemplate=new RestTemplate();
       FakeStoreProductDto fakeStoreProductDto =  restTemplate.getForObject(
-                'https://fakestoreapi.com/products/' + productId, FakeStoreProductDto.class
+                "https://fakestoreapi.com/products/" + productId, FakeStoreProductDto.class
         );
+
       //finally converting the FakeStoreProductDto object to product object.
       return convertFakeStoreProductToProduct(fakeStoreProductDto);
     }
@@ -35,7 +36,7 @@ public class FakeStoreProductService implements ProductService { // so here comp
 
     //using a method as for the conversion to Product as this conversion need multiple times.
     private Product convertFakeStoreProductToProduct(FakeStoreProductDto fakeStoreProductDto){
-        product product =new Product();
+        Product product =new Product();
         product.setId(fakeStoreProductDto.getId());
         product.setTitle(fakeStoreProductDto.getTitle());
         product.setPrice(fakeStoreProductDto.getPrice());
@@ -44,6 +45,6 @@ public class FakeStoreProductService implements ProductService { // so here comp
         category.setDescription(fakeStoreProductDto.getCategory()); //as category is an object in product object so we created the category object and the data we are getting from the fakeStoreProductDto objetc is string of description so we set the description of category object from the fakeStoreProductDto category and lastly we set the category that category to product object.
         product.setCategory(category);
 
-        return  product();
+        return  product;
     }
 }
